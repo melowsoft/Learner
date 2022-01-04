2aa154012fb7faefa3920b9814c7eadecead4889
Anthony Okpako <melowsworld@yahoo.co.uk>
2022-01-04 22:29:50 +0100

    Initial Commit

---
+++ Shared/LearnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnerApp.swift
+//  Shared
+//
+//  Created by Tony on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
